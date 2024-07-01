bd6e0dc3ca539d475b10bd51abf0cfe49153ebf6
gopimekala230195 <gopi.mekala@eagles.oc.edu>
2024-07-01 09:42:15 -0500

    initial_commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MenuBar from "./components/MenuBar.vue";
+import careerServicesBG from "./images/career-bg.jpeg";
+</script>
+
+<template>
+  <v-app :style="{
+    'background-image': `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
+                  url(${careerServicesBG})`,
+    'background-position': 'center',
+    'background-repeat': 'no-repeat',
+    'background-size': 'cover',
+    'color': 'white',
+  }">
+    <MenuBar :key="$route.fullPath" />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
