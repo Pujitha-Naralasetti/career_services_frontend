a9ddb359170e4bd3b875bc8eaf35aae49a367f27
Anusha <anushaveeragoni@gmail.com>
2024-07-02 18:03:27 -0500

    dashboard_changes

--- src/router.js
+++ src/router.js
@@ -8,7 +8,11 @@ const router = createRouter({
       name: "login",
       component: () => import("./views/Login.vue"),
     },
-
+    {
+      path: "/Dashboard",
+      name: "dashboard",
+      component: () => import("./views/Dashboard.vue"),
+    },
   ],
 });
 
